743d87bad1fc504ccdd53ab8b4c27cefb40aa169
mijacke <lassumario2002@gmail.com>
2024-03-18 15:24:05 +0100

    updated BussManager .h

--- Assignment/BusStopManager.h
+++ Assignment/BusStopManager.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#include "BusStop.h"
+#include <vector>
+#include <functional>
+
 class BusStopManager
 {
+private:
+	std::vector<BusStop> busStops;
+
+public:
+	void loadFromCSV(const std::string& filename);
+	std::vector<BusStop> filterBusStops(std::function<bool(const BusStop&)> predicate) const;
 };
 
