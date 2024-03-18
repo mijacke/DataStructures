fdf672138cc821ca1d385ba4b22f7daa8548fc6a
mijacke <lassumario2002@gmail.com>
2024-03-18 15:27:48 +0100

    updated method

--- Assignment/BusStopManager.cpp
+++ Assignment/BusStopManager.cpp
@@ -1,1 +1,24 @@
 #include "BusStopManager.h"
+
+#include <fstream>
+
+void BusStopManager::loadFromCSV(const std::string& filename)
+{
+	std::ifstream file(filename);
+	std::string line;
+	std::getline(file, line); // skip header line
+
+	while (std::getline(file, line))
+	{
+		//stopid; stopname; stopsite; latitude; longitude; syscode; system; municipality
+		std::stringstream s(line);
+		std::string stopid;
+		std::string stopname;
+		std::string stopsite;
+		double latitude;
+		double longitude;
+		std::string syscode;
+		std::string system;
+		std::string municipality;
+	}
+}
