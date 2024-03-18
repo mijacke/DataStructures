088b1e2a66363dff862c0ce1c41d07917a815a7b
mijacke <lassumario2002@gmail.com>
2024-03-18 15:13:56 +0100

    added BusStop constructor

--- Assignment/BusStop.cpp
+++ Assignment/BusStop.cpp
@@ -1,1 +1,9 @@
 #include "BusStop.h"
+
+BusStop::BusStop(const std::string& stopId, const std::string& name, const std::string& stopSite,
+	double latitude, double longitude, const std::string& sysCode, 
+	const std::string& system, const std::string& municipality) 
+	: stopId(stopId), name(name), stopSite(stopSite), latitude(latitude), longitude(longitude), 
+	sysCode(sysCode), system(system), municipality(municipality) {}
+
+
