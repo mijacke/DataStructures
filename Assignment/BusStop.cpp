03e3c8b3e05efc450285c3f2dae06ca020cad08f
mijacke <lassumario2002@gmail.com>
2024-03-18 15:15:20 +0100

    Added other methods

--- Assignment/BusStop.cpp
+++ Assignment/BusStop.cpp
@@ -6,4 +6,48 @@ BusStop::BusStop(const std::string& stopId, const std::string& name, const std::
 	: stopId(stopId), name(name), stopSite(stopSite), latitude(latitude), longitude(longitude), 
 	sysCode(sysCode), system(system), municipality(municipality) {}
 
+BusStop::~BusStop()
+{
+}
+
+const std::string& BusStop::getStopId() const
+{
+	// TODO: insert return statement here
+}
+
+const std::string& BusStop::getName() const
+{
+	// TODO: insert return statement here
+}
+
+const std::string& BusStop::getStopSite() const
+{
+	// TODO: insert return statement here
+}
+
+double BusStop::getLatitude() const
+{
+	return 0.0;
+}
+
+double BusStop::getLongitude() const
+{
+	return 0.0;
+}
+
+const std::string& BusStop::getSysCode() const
+{
+	// TODO: insert return statement here
+}
+
+const std::string& BusStop::getSystem() const
+{
+	// TODO: insert return statement here
+}
+
+const std::string& BusStop::getMunicipality() const
+{
+	// TODO: insert return statement here
+}
+
 
